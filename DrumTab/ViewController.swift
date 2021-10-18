958c7773e4ca9ee6408c724c0d228027849f8367
Ivan Lai <229705jack@gmail.com>
2021-10-18 10:45:43 +0800

    Environment setting done.

---
+++ DrumTab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrumTab
+//
+//  Created by 賴柏穎 on 2021/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
